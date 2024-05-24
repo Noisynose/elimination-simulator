258762d1e71026672d92ecce500366b3daad5ab3
Alexandre Consigny <alexandre.consigny@gmail.com>
2024-05-23 21:54:00 -0400

    feat: add constructor arguments

--- src/main.gd
+++ src/main.gd
@@ -1,2 +1,7 @@
+var _something: int
+
+func _init(something: int = 37):
+  _something = something
+
 func a_function() -> int:
   return 0
--- src/main.test.gd
+++ src/main.test.gd
@@ -2,9 +2,17 @@ extends GutTest
 var mainModule = load("res://src/main.gd")
 
 func test_given_a_function_when_return_then_equals_0():
-  var main = mainModule.new()
+  var instance = mainModule.new()
 
-  var result: int = main.a_function()
+  var result: int = instance.a_function()
 
   var expected_result: int = 0
+  assert_eq(result, expected_result)
+
+func test_given_an_attribute_when_get_value_then_equals_init_value():
+  var instance = mainModule.new(123)
+
+  var result: int = instance.something
+
+  var expected_result: int = 123
   assert_eq(result, expected_result)
